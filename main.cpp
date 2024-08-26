2ee712e5057851afdde164248abf4f190cc39e41
Jonathan Peters <jmp22@sfu.ca>
2024-08-25 21:10:37 -0700

    edit scene

--- main.cpp
+++ main.cpp
@@ -11,14 +11,16 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_blue_plastic = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_red_plastic = make_shared<lambertian>(color(0.8, 0.1, 0.1));
-    auto material_metal = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_orange_metal = make_shared<metal>(color(0.8, 0.4, 0.1));
+    auto material_blue_plastic = make_shared<lambertian>(color(0.4, 0.3, 0.5));
+    auto material_red_plastic = make_shared<lambertian>(color(0.85, 0.5, 0.1));
+    auto material_metal = make_shared<metal>(color(0.8, 0.8, 0.8), 0.03);
+    auto material_orange_metal = make_shared<metal>(color(0.8, 0.4, 0.1), 0.1);
 
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, material_orange_metal));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_metal));
-    world.add(make_shared<sphere>(point3(0,100.5,-1), 100, material_metal));
+    world.add(make_shared<sphere>(point3(0,0,-1), 2, material_blue_plastic));
+    //world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_metal));
+    //world.add(make_shared<sphere>(point3(0,100.5,-1), 100, material_metal));
+    //world.add(make_shared<sphere>(point3(100.5,0,-1), 100, material_metal));
+    //world.add(make_shared<sphere>(point3(-100.5,0,-1), 100, material_metal));
 
     camera cam;
 
@@ -27,7 +29,7 @@ int main() {
     cam.samples_per_pixel = 10;
 
     cam.set_anti_aliasing(true);
-    cam.set_focal_length(1);
+    cam.set_focal_length(0.85);
     cam.max_depth = 50;
 
     cam.render(world);
--- material.h
+++ material.h
@@ -20,7 +20,7 @@ class lambertian : public material {
         lambertian(const color& albedo) : albedo(albedo) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
-            auto scatter_direction = rec.normal + 0*random_unit_vector();
+            auto scatter_direction = rec.normal + random_unit_vector();
 
             if (scatter_direction.near_zero()) {
                 scatter_direction = rec.normal;
@@ -38,17 +38,19 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& albedo) : albedo(albedo) {}
+        metal(const color& albedo, double rough) : albedo(albedo), rough(rough < 1 ? rough : 1) {}
         
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
             vec3 reflected = reflect(r_in.direction(), rec.normal);
+            reflected = unit_vector(reflected) + (rough * random_unit_vector());
             scattered = ray(rec.p, reflected);
             attenuation = albedo;
-            return true;
+            return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     private:
         color albedo;
+        double rough;
 };
 
 #endif
